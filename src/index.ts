be9ac63b15b4aa4f92eac590b380c6fa31358b36
Isaiah Anyimi <41877924+ianyimi@users.noreply.github.com>
2025-10-27 18:21:20 -0700

    fix: remove invalid import from index.ts file that is breaking projects

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import type { Config } from 'payload'
 import type { RedisPluginConfig } from './types.js'
 
 import { dbAdapterWithCache } from './adapter.js'
-import './extendedTypes.js'
 
 export const redisCache =
 	(pluginConfig: RedisPluginConfig) =>
